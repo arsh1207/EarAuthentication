b8b7bbf093afd1ca20e4fbebc6431805c96e0c17
Arsalaan Javed <arsalaankhan1207@gmail.com>
2019-11-23 16:03:46 -0500

    “added_Lem”

--- Preprocessing.py
+++ Preprocessing.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 # from PIL import Image
 from matplotlib import pyplot as plt
+from sklearn.manifold import SpectralEmbedding
 import math
 
 
@@ -21,6 +22,8 @@ class Preprocessing:
         # plt.show()
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray = cv2.resize(gray, dsize=(100, 150), interpolation=cv2.INTER_NEAREST)
+        # plt.imshow(gray)
+        # plt.show()
         gray_ = gray
         mean = cv2.mean(gray)[0]
         variance = np.var(gray)
@@ -40,14 +43,28 @@ class Preprocessing:
 
     # in this function we are creating sw of size 50 x 50 with step size 11 so total 50 SWs
     def sub_window_creation(self, image, kernels):
+        gb_all_sw = []
         for i in range(0, 100, 11):
             for j in range(0, 50, 11):
-                gabored_image = Preprocessing.process(self, image[i:i+50, j:j+50], kernels)
-                # print(gabored_image.shape)
-                plt.imshow(image[i:i+50, j:j+50], cmap='gray')
-                plt.show()
-                plt.imshow(gabored_image, cmap='gray')
-                plt.show()
+                sw_image = image[i:i+50, j:j+50]
+                sw_image = cv2.resize(sw_image, dsize=(12, 12), interpolation=cv2.INTER_NEAREST)
+                print('sw size', sw_image.shape)
+                gabored_image = Preprocessing.process(self, sw_image, kernels)
+                print('gab size', gabored_image.shape)
+                # print('gab size', gabored_image.reshape(1, -1).shape)
+                gb_all_sw.append(gabored_image)
+                # gb_all_sw.append(reduced_sw)
+                # print('gab size', reduced_sw.shape)
+                # plt.imshow(image[i:i+50, j:j+50], cmap='gray')
+                # plt.show()
+                # plt.imshow(gabored_image, cmap='gray')
+                # plt.show()
+        print(len(gb_all_sw[0]))
+        model = SpectralEmbedding(n_components=100, n_neighbors=10)
+        # reduced_sw = model.fit_transform(gabored_image.reshape(-1, 1))
+        reduced_sw = model.fit_transform(gb_all_sw)
+        print('final', len(reduced_sw))
+        print('final', len(reduced_sw[0]))
 
     # creating gabor kernel bank
     def gabor_filter(self):
@@ -63,11 +80,13 @@ class Preprocessing:
         return kernels
 
     def process(self, img, filters):
+        gabored_images = np.array([])
         accum = np.zeros_like(img)
         for kern in filters:
             fimg = cv2.filter2D(img, cv2.CV_8UC3, kern)
-        np.maximum(accum, fimg, accum)
-        return accum
+            gabored_images = np.append(gabored_images, fimg)
+        # np.maximum(accum, fimg, accum)
+        return gabored_images
 
 
 obj = Preprocessing()
